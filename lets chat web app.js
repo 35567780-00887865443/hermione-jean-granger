3f44521036dbfaa3ebb78e49eb851c0ba12a5b7d
35567780-00887865443 <91005748+35567780-00887865443@users.noreply.github.com>
2022-02-09 13:24:27 +0530

    Add files via upload

---
+++ lets chat web app.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAR90rR5rkogu-3BV4Ov775Vz29dhkP0-I",
+  authDomain: "kwitter-566a1.firebaseapp.com",
+  projectId: "kwitter-566a1",
+  storageBucket: "kwitter-566a1.appspot.com",
+  messagingSenderId: "57657763299",
+  appId: "1:57657763299:web:fe5fa7a646d16c1408ecd6"
+};
+
+// Initialize Firebase
+function getData(){firebase.database().ref("/").on('value',function(snapshot){document.getElementById("output").innerHTML
+   group_name= childKey;                                                                         
+                                                                             })}
+getData();
